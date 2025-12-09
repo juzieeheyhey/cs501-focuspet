3b22ac00f2e9f13a7d868d8c02a7491aa030a694
Tanish Bhowmick <tanishbhowmick@gmail.com>
2025-12-09 12:53:11 -0500

    fixing accidental removal of active-win api

--- frontend/preload.js
+++ frontend/preload.js
@@ -4,6 +4,15 @@ import { contextBridge, ipcRenderer } from 'electron';
 const BACKEND_BASE = process.env.BACKEND_BASE || 'http://localhost:5185';
 contextBridge.exposeInMainWorld('BACKEND_BASE', BACKEND_BASE);
 
+contextBridge.exposeInMainWorld('electronAPI', {
+    onActiveWindow: (cb) => {
+        ipcRenderer.on('active-window', (_event, info) => cb(info));
+    },
+    requestStartPolling: () => ipcRenderer.send('active-window-start'),
+    requestStopPolling: () => ipcRenderer.send('active-window-stop'),
+    invokeGetActiveWindow: () => ipcRenderer.invoke('get-active-window'),
+});
+
 contextBridge.exposeInMainWorld('lastSessionPopup', {
     open: (payload) => ipcRenderer.send('last-session-window:openWindow', payload),
     onLoad: (cb) => {
